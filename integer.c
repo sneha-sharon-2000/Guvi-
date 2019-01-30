ab0760c7e0d9cdc72369f0108a89fceb18f9eade
sneha-sharon-2000 <43330827+sneha-sharon-2000@users.noreply.github.com>
2019-01-30 14:01:48 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+{
+int num;
+printf("enter the number");
+scanf("%d",num);
+if(num>0)
+printf("the number is positive",num);
+else if(num<0)
+printf("the number is negative",num);
+else
+printf("the number is neither postivr or negative");
+}
+
