22924050a21d5595dba705f7a0f77fa50023b892
sneha-sharon-2000 <43330827+sneha-sharon-2000@users.noreply.github.com>
2019-01-30 14:52:43 +0530

    Create vowel or consonants.c

---
+++ vowel or consonants.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#niclude<conio.h>
+int main();
+{
+char c;
+int isLowercasevowel ,isUppercase vowel;
+printf("enter an alpha");
+scanf("%c",&c);
+isLowercasevowel=(c=='a'||c=='b'||c=='e');
+isUppercasevowel=(c=='A'||c=='B'||c=='E')'
+if (isLowercasevowel||isUppercasevowel)
+printf("%c is vowel",c);
+else
+printf("%c is consonants",c);
+}
