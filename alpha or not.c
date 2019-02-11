13221bede0361513511edcc5a7457aa8b28ab5b5
sneha-sharon-2000 <43330827+sneha-sharon-2000@users.noreply.github.com>
2019-02-11 10:52:52 +0530

    Create alpha or not.c

---
+++ alpha or not.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char c;
+printf("enter a character");
+scanf("%c",&c);
+if((c>='a' && c<='z')||(c<='A' && c<='Z'));
+printf("%c is a alphabet",c);
+else
+printf("%c is not a alphabet",c);
+}
