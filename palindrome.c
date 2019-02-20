41048165c8881c96c5c562cd96732292695c525c
sneha-sharon-2000 <43330827+sneha-sharon-2000@users.noreply.github.com>
2019-02-20 14:39:31 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+int n,a,remainder,b;
+printf("enter the integer");
+scanf("%d",&n);
+a=n;
+whlie(n!=0)
+{
+remainder=n%10;
+b=b*10 = remainder;
+n/=10;
+if (a==b)
+printf("%d is palindrome",&a);
+else
+printf(""%d is not palindrome",&a);
+}
+getch();
+}
