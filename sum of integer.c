60b4fe38a9fc0ce0edefc3b4b0ea1fb53f92fe7e
sneha-sharon-2000 <43330827+sneha-sharon-2000@users.noreply.github.com>
2019-02-20 14:04:38 +0530

    Update sum of integer.c

--- sum of integer.c
+++ sum of integer.c
@@ -1,3 +1,25 @@
 #include<stdio.h>
 #include<conio.h>
 void main()
+{
+  int n,k,i;
+  cin>>n>>k;
+  int a[n],b[k];
+for(int i=0; i<k; i++)
+ b[i]=0;
+ for(int i = 0; i < n; i++)
+{
+cin >> a[i];
+b[a[i]%k]++;
+}
+int sum=0;
+sum+=(b[0]*(b[0]-1))/2;
+for(int i=1; i<=k/2 && i!=k-i; i++)
+{
+sum+=b[i]*b[k-i];
+}
+if(k%2==0)
+sum+=(b[k/2]*(b[k/2]-1))/2;
+cout<<sum;
+getch();
+}
